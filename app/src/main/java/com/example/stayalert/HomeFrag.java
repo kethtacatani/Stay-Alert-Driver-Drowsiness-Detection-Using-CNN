3cfe7e79315af995b6a6459066b070fa350de201
Keth Dominic Tacatani <115775088+kethtacatani@users.noreply.github.com>
2023-10-31 21:55:48 +0800

    integrated detection

--- app/src/main/java/com/example/stayalert/HomeFrag.java
+++ app/src/main/java/com/example/stayalert/HomeFrag.java
@@ -2,11 +2,14 @@ package com.example.stayalert;
 
 import android.os.Bundle;
 
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 ///**
 // * A simple {@link Fragment} subclass.
@@ -15,11 +18,25 @@ import android.view.ViewGroup;
 // */
 public class HomeFrag extends Fragment {
 
+    Button viewDetectionBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_, container, false);
+
+        // Initialize the viewDetectionBtn by finding it from the inflated layout
+        viewDetectionBtn = view.findViewById(R.id.viewDetectionBtn);
+
+        // Set an OnClickListener for the viewDetectionBtn
+        viewDetectionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CameraActivity cameraActivity = (CameraActivity) getActivity();
+                cameraActivity.changeFrameLayoutElevation(30);
+            }
+        });
+
+        return view;
     }
 }
